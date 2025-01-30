e63e15fd6dee51f31dcf674ee9491ccdc9a5daa8
Dqrk <staurospapadogiannopoulos@gmail.com>
2025-01-30 15:27:41 +0000

    master

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
+import { createClient } from 'redis';
 
+const client = createClient({
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    socket: {
+        host: process.env.HOST,
+        port: process.env.PORT
+    }
+});
+
+client.on('error', (err) => {
+    console.log('Redis Client Error', err)
+});
+
+await client.connect();
